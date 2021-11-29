7f923456dcf32dfd6166a10040f9dffad1100f68
eliaskone <56181285+eliaskone@users.noreply.github.com>
2021-11-29 13:38:28 +0100

    Add files via upload

---
+++ yt_gui.sh
@@ -0,0 +1,25 @@
+#!bin/bash
+
+DOWNDIR=$HOME/Videos/youtube/
+mkdir $DOWNDIR
+
+EINGABE=$(zenity --list --title "Youtube-DLH" --text "Format auswahl" \
+--column "Auswahl" --column "Typ" --radiolist TRUE "Video herunterladen" \
+FALSE "Audio herunterladen" --width=600 --height=400)
+
+if [ "$EINGABE" == "Video herunterladen" ]
+then
+	LINKVID=$(zenity --entry --title "Link einfügen" --text "Link zum Youtube Video:" --width=600)
+
+	yt-dlp -o "$DOWNDIR%(title)s.%(ext)s" $LINKVID | zenity --progress --title "Fortschrittsanzeige" --text "Lade herunter" --pulsate && exit 0
+
+fi
+
+
+if [ "$EINGABE" == "Audio herunterladen" ]
+then
+	LINKAUD=$(zenity --entry --title "Link einfügen" --text "Link zum Youtube Video:" --width=600)
+
+	yt-dlp -o "$DOWNDIR%(title)s.%(ext)s" -x --audio-format mp3 $LINKAUD | zenity --progress --title "Fortschrittsanzeige" --text "Lade herunter" --pulsate && exit 0
+
+fi
